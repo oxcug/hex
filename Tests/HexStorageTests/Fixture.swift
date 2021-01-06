cb6b7d0133c149a2223048440ac12e0a690096f5
oxcug <oxcug@pm.me>
2021-01-06 12:11:54 -0700

    updated tests to include integer typed (nonnullable and nullable) field.

--- Tests/HexStorageTests/Fixture.swift
+++ Tests/HexStorageTests/Fixture.swift
@@ -16,7 +16,14 @@ class ExampleModel: Model, Equatable {
     }
 
     static func ==(lhs: ExampleModel, rhs: ExampleModel) -> Bool {
-        return lhs.string == rhs.string
+        return lhs.string == rhs.string &&
+                lhs.date == rhs.date &&
+                lhs.double == rhs.double &&
+                lhs.integer == rhs.integer &&
+                lhs.nullableString == rhs.nullableString &&
+                lhs.nullableDate == rhs.nullableDate &&
+                lhs.nullableDouble == rhs.nullableDouble &&
+                lhs.nullableInteger == rhs.nullableInteger
     }
     
     override class func migrate<T>(using current: ModelMigrationBuilder<T>) -> ModelOperation<T>? where T : RawModel {
@@ -25,9 +32,11 @@ class ExampleModel: Model, Equatable {
                     .attribute(.string, named: "string"),
                     .attribute(.date, named: "date"),
                     .attribute(.float, named: "double"),
+                    .attribute(.integer, named: "integer"),
                     .attribute(.string, named: "nullableString"),
                     .attribute(.date, named: "nullableDate"),
-                    .attribute(.float, named: "nullableDouble")
+                    .attribute(.float, named: "nullableDouble"),
+                    .attribute(.integer, named: "nullableInteger")
             )
         )
     }
@@ -37,10 +46,14 @@ class ExampleModel: Model, Equatable {
     @Attribute(defaultValue: .distantFuture) var date: Date
     
     @Attribute(defaultValue: 99) var double: Double
+
+    @Attribute(defaultValue: 1) var integer: Int
     
     @NullableAttribute var nullableString: String?
     
     @NullableAttribute var nullableDate: Date?
     
     @NullableAttribute var nullableDouble: Double?
+
+    @NullableAttribute var nullableInteger: Int?
 }
